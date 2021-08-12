0ed59c1774613c4359e972f8fc0b6be540ffc048
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-08-12 22:34:05 +0100

    chore(install-hooks.bash): Fix comment

--- git-hooks/install-hooks.bash
+++ git-hooks/install-hooks.bash
@@ -8,8 +8,8 @@ GIT_DIR=$(git rev-parse --git-dir)
 chmod +x pre-commit.py commit-msg.py pre-push.bash post-checkout.py
 
 echo "Installing hooks..."
-# symlinks to the pre-commit script
 git config --local commit.template $HOOK_DIR/commit_template.txt
+# symlinks to the hook scripts
 ln -sf $GIT_DIR/../$HOOK_DIR/pre-commit.py $GIT_DIR/hooks/pre-commit
 ln -sf $GIT_DIR/../$HOOK_DIR/commit-msg.py $GIT_DIR/hooks/commit-msg
 ln -sf $GIT_DIR/../$HOOK_DIR/pre-push.bash $GIT_DIR/hooks/pre-push
