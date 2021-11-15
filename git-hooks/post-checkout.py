ddc44b5e34f0029d98624762893b561f8c4f1552
R, Alex <alex.richardson19@imperial.ac.uk>
2021-11-15 11:14:05 +0000

    refactor(git-hooks/): Extract code to `print_successful`

--- git-hooks/post-checkout.py
+++ git-hooks/post-checkout.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+# pylint: disable=C0103
+"""
+post-checkout hook is used to perform repository validity checks
+"""
 
 import os
 import sys
-from colorama import Fore, Style
+from util import print_successful
 
 
 def main():
+    """Main function for the post-checkout hook."""
     print("--- Running post-checkout hook ---")
-    CLEAN_COMMAND = ""
-    os.system(CLEAN_COMMAND)  # clean command here e.g. `make clean`
-    print(Fore.GREEN + Style.BRIGHT +
-          "post-checkout hook finished successfully." + Style.RESET_ALL)
+    command = ""
+    os.system(command)  # clean command here e.g. `make clean`
+    print_successful("post-checkout")
     print("-------------------------------")
     sys.exit(0)
 
--- git-hooks/util.py
+++ git-hooks/util.py
@@ -6,6 +6,12 @@ import sys
 from colorama import Fore, Style
 
 
+def print_successful(hook_name):
+    """Print that `hook_name` has finished successfully."""
+    print(Fore.GREEN + Style.BRIGHT +
+          f"{hook_name} hook finished successfully." + Style.RESET_ALL)
+
+
 def exit_failure(error_message):
     """Exits non-zero with error message."""
     print(
